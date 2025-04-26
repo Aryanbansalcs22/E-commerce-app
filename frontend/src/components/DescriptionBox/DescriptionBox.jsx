702e7590e1c4ab43767b02c8ec3a51af941b96a1
deepanshujain04 <jaindeepanshu2004@gmail.com>
2025-04-26 16:35:22 +0530

    Added new files

---
+++ frontend/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>An e-commerce website is an online platform that allows businesses and individuals to buy and sell products or services over the internet. It provides a seamless
+                     shopping experience with features like product listings, secure payment gateways, user-friendly navigation, and customer reviews. E-commerce sites can range
+                      from small niche stores to large marketplaces, offering convenience, accessibility, and a wide variety of products to customers worldwide.An e-commerce website
+                       is a digital storefront that enables businesses to showcase and sell products or services online. These websites provide customers with a seamless shopping experience 
+                       by offering product categories, search filters, shopping carts, and secure checkout options. Modern e-commerce platforms also integrate payment gateways, shipping options,
+                        and customer support to enhance user satisfaction.</p>
+                      <p>E-commerce sites can be B2C (Business-to-Consumer), B2B (Business-to-Business), or C2C (Consumer-to-Consumer), catering to different markets. With mobile-friendly 
+                        designs, AI-driven recommendations, and personalized user experiences, e-commerce continues to revolutionize the way people shop. Whether selling fashion, electronics,
+                         or digital services, an e-commerce website is essential for businesses looking to grow and reach a global audience
+                         </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default DescriptionBox
